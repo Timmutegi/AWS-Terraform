99dce35e746a0307eb50f0673ff0e404ce17b3ee
Timmutegi <mutegi.timothy538@gmail.com>
2022-04-21 16:34:48 -0600

    Added AWS authentication variables

--- iac/variable.tf
+++ iac/variable.tf
@@ -1,11 +1,24 @@
 variable "region" {
+  type = string
   default = "us-west-1"
 }
 
 variable "resource_name" {
+  type = string
   default = "file"
 }
 
 variable "stage" {
+  type = string
   default = "dev"
+}
+
+#AWS authentication variables
+variable "aws_access_key" {
+  type = string
+  description = "AWS Access Key"
+}
+variable "aws_secret_key" {
+  type = string
+  description = "AWS Secret Key"
 }
