77239bc5cce1bb5f9802f44a23ed78c7d5852781
Timmutegi <mutegi.timothy538@gmail.com>
2022-04-21 09:26:56 -0600

    Added buckt name to outputs

--- iac/outputs.tf
+++ iac/outputs.tf
@@ -1,3 +1,7 @@
-# output "base_url" {
-#   value = aws_api_gateway_deployment.gateway_deployment.invoke_url
-# }
+output "base_url" {
+  value = "${aws_api_gateway_deployment.gateway_deployment.invoke_url}/${var.resource_name}"
+}
+
+output "bucket_name" {
+  value = "${aws_s3_bucket.bucket.id}"
+}
