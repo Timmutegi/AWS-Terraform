b241301e8969930466f0332bcd717c3b2b9a6eae
Timmutegi <mutegi.timothy538@gmail.com>
2022-04-21 09:27:18 -0600

    Updated lambda function folder path

--- iac/main.tf
+++ iac/main.tf
@@ -74,14 +74,14 @@ resource "aws_iam_role_policy_attachment" "test-attach" {
 
 data "archive_file" "zip_the_python_code" {
   type        = "zip"
-  source_dir  = "${path.module}/lambda2/"
-  output_path = "${path.module}/lambda2/hello-python.zip"
+  source_dir  = "${path.module}/lambda_function/"
+  output_path = "${path.module}/lambda_function/hello-python.zip"
 }
 
 resource "aws_lambda_function" "lambda_function" {
   # If the file is not in the current working directory you will need to include a 
   # path.module in the filename.
-  filename      = "${path.module}/lambda2/hello-python.zip"
+  filename      = "${path.module}/lambda_function/hello-python.zip"
   function_name = "AclarionFunction"
   role          = aws_iam_role.iam_for_lambda.arn
   handler       = "handler.lambda_handler"
@@ -93,24 +93,23 @@ resource "aws_api_gateway_rest_api" "rest_api" {
   description = "This API is an event source for the lambda function"
 }
 
-# ****************************************************************************************************************
-resource "aws_api_gateway_resource" "number" {
+resource "aws_api_gateway_resource" "file" {
   parent_id   = aws_api_gateway_rest_api.rest_api.root_resource_id
   rest_api_id = aws_api_gateway_rest_api.rest_api.id
   path_part   = "{${var.resource_name}+}"
 }
 
-resource "aws_api_gateway_method" "number" {
+resource "aws_api_gateway_method" "file" {
   rest_api_id   = aws_api_gateway_rest_api.rest_api.id
-  resource_id   = aws_api_gateway_resource.number.id
+  resource_id   = aws_api_gateway_resource.file.id
   http_method   = "GET"
   authorization = "NONE"
 }
 
 resource "aws_api_gateway_integration" "lambdapy" {
   rest_api_id = aws_api_gateway_rest_api.rest_api.id
-  resource_id = aws_api_gateway_method.number.resource_id
-  http_method = aws_api_gateway_method.number.http_method
+  resource_id = aws_api_gateway_method.file.resource_id
+  http_method = aws_api_gateway_method.file.http_method
 
   integration_http_method = "POST"
   type                    = "AWS"
@@ -120,13 +119,13 @@ resource "aws_api_gateway_integration" "lambdapy" {
   request_templates = {
     "application/json" = <<EOF
     {
-      "hour" : $util.urlDecode($input.params('hour'))
+      "url" : $util.urlDecode($input.params('url'))
     }
     EOF
   }
 }
 
-resource "aws_api_gateway_method" "number_rootpy" {
+resource "aws_api_gateway_method" "file_rootpy" {
   rest_api_id   = aws_api_gateway_rest_api.rest_api.id
   resource_id   = aws_api_gateway_rest_api.rest_api.root_resource_id
   http_method   = "GET"
@@ -135,8 +134,8 @@ resource "aws_api_gateway_method" "number_rootpy" {
 
 resource "aws_api_gateway_integration" "lambda_rootpy" {
   rest_api_id = aws_api_gateway_rest_api.rest_api.id
-  resource_id = aws_api_gateway_method.number_rootpy.resource_id
-  http_method = aws_api_gateway_method.number_rootpy.http_method
+  resource_id = aws_api_gateway_method.file_rootpy.resource_id
+  http_method = aws_api_gateway_method.file_rootpy.http_method
 
   integration_http_method = "POST"
   type                    = "AWS"
@@ -145,8 +144,8 @@ resource "aws_api_gateway_integration" "lambda_rootpy" {
 
 resource "aws_api_gateway_method_response" "response_200" {
   rest_api_id = aws_api_gateway_rest_api.rest_api.id
-  resource_id = aws_api_gateway_resource.number.id
-  http_method = aws_api_gateway_method.number.http_method
+  resource_id = aws_api_gateway_resource.file.id
+  http_method = aws_api_gateway_method.file.http_method
   status_code = "200"
 
   response_models = { "application/json" = "Empty" }
@@ -158,8 +157,8 @@ resource "aws_api_gateway_integration_response" "IntegrationResponse" {
     aws_api_gateway_integration.lambda_rootpy,
   ]
   rest_api_id = aws_api_gateway_rest_api.rest_api.id
-  resource_id = aws_api_gateway_resource.number.id
-  http_method = aws_api_gateway_method.number.http_method
+  resource_id = aws_api_gateway_resource.file.id
+  http_method = aws_api_gateway_method.file.http_method
   status_code = aws_api_gateway_method_response.response_200.status_code
   # Transforms the backend JSON response to json. The space is "A must have"
   response_templates = {
@@ -177,63 +176,6 @@ resource "aws_api_gateway_deployment" "gateway_deployment" {
   rest_api_id = aws_api_gateway_rest_api.rest_api.id
   stage_name  = var.stage
 
-}
-
-# ********************************************************************************************************************
-# resource "aws_api_gateway_resource" "proxy" {
-#   rest_api_id = aws_api_gateway_rest_api.rest_api.id
-#   parent_id   = aws_api_gateway_rest_api.rest_api.root_resource_id
-#   path_part   = "{proxy+}"
-# }
-
-# resource "aws_api_gateway_method" "proxy" {
-#   rest_api_id   = aws_api_gateway_rest_api.rest_api.id
-#   resource_id   = aws_api_gateway_resource.proxy.id
-#   http_method   = "ANY"
-#   authorization = "NONE"
-# }
-
-# resource "aws_api_gateway_integration" "lambda" {
-#   rest_api_id = aws_api_gateway_rest_api.rest_api.id
-#   resource_id = aws_api_gateway_method.proxy.resource_id
-#   http_method = aws_api_gateway_method.proxy.http_method
-
-#   integration_http_method = "POST"
-#   type                    = "AWS_PROXY"
-#   uri                     = aws_lambda_function.lambda_function.invoke_arn
-# }
-
-# resource "aws_api_gateway_method" "proxy_root" {
-#   rest_api_id   = aws_api_gateway_rest_api.rest_api.id
-#   resource_id   = aws_api_gateway_rest_api.rest_api.root_resource_id
-#   http_method   = "ANY"
-#   authorization = "NONE"
-# }
-
-# resource "aws_api_gateway_integration" "lambda_root" {
-#   rest_api_id = aws_api_gateway_rest_api.rest_api.id
-#   resource_id = aws_api_gateway_method.proxy_root.resource_id
-#   http_method = aws_api_gateway_method.proxy_root.http_method
-
-#   integration_http_method = "POST"
-#   type                    = "AWS_PROXY"
-#   uri                     = aws_lambda_function.lambda_function.invoke_arn
-# }
-
-# resource "aws_api_gateway_deployment" "gateway_deployment" {
-#   depends_on = [
-#     aws_api_gateway_integration.lambda,
-#     aws_api_gateway_integration.lambda_root,
-#   ]
-
-#   rest_api_id = aws_api_gateway_rest_api.rest_api.id
-#   stage_name  = "test"
-# }
-
-# *************************************************************************************************
-
-output "base_url" {
-  value = "${aws_api_gateway_deployment.gateway_deployment.invoke_url}/${var.resource_name}"
 }
 
 resource "aws_lambda_permission" "lambda_permission" {
