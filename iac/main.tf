1ed4140fa090b52cb6053e13cb50334b3f9826de
Timmutegi <mutegi.timothy538@gmail.com>
2022-04-21 08:09:48 -0600

    Added Terraform configuration files

---
+++ iac/main.tf
@@ -0,0 +1,279 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = var.region
+}
+
+resource "aws_s3_bucket" "bucket" {
+  bucket = "aclarion-bucket-1"
+}
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "iam_for_lambda"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+#Create Policy for IAM Role
+resource "aws_iam_policy" "policy" {
+  name        = "lambda_policy"
+  description = "A test policy"
+
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+          "logs:*"
+      ],
+      "Resource": "arn:aws:logs:*:*:*"
+    },
+    {
+        "Effect": "Allow",
+        "Action": [
+            "s3:*"
+        ],
+        "Resource": "arn:aws:s3:::*"
+    }
+  ]
+} 
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "test-attach" {
+  role       = aws_iam_role.iam_for_lambda.name
+  policy_arn = aws_iam_policy.policy.arn
+}
+
+data "archive_file" "zip_the_python_code" {
+  type        = "zip"
+  source_dir  = "${path.module}/lambda2/"
+  output_path = "${path.module}/lambda2/hello-python.zip"
+}
+
+resource "aws_lambda_function" "lambda_function" {
+  # If the file is not in the current working directory you will need to include a 
+  # path.module in the filename.
+  filename      = "${path.module}/lambda2/hello-python.zip"
+  function_name = "AclarionFunction"
+  role          = aws_iam_role.iam_for_lambda.arn
+  handler       = "handler.lambda_handler"
+  runtime       = "python3.9"
+}
+
+resource "aws_api_gateway_rest_api" "rest_api" {
+  name        = "AclarionAPI"
+  description = "This API is an event source for the lambda function"
+}
+
+# ****************************************************************************************************************
+resource "aws_api_gateway_resource" "number" {
+  parent_id   = aws_api_gateway_rest_api.rest_api.root_resource_id
+  rest_api_id = aws_api_gateway_rest_api.rest_api.id
+  path_part   = "{${var.resource_name}+}"
+}
+
+resource "aws_api_gateway_method" "number" {
+  rest_api_id   = aws_api_gateway_rest_api.rest_api.id
+  resource_id   = aws_api_gateway_resource.number.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambdapy" {
+  rest_api_id = aws_api_gateway_rest_api.rest_api.id
+  resource_id = aws_api_gateway_method.number.resource_id
+  http_method = aws_api_gateway_method.number.http_method
+
+  integration_http_method = "POST"
+  type                    = "AWS"
+  uri                     = aws_lambda_function.lambda_function.invoke_arn
+  passthrough_behavior    = "WHEN_NO_TEMPLATES"
+
+  request_templates = {
+    "application/json" = <<EOF
+    {
+      "hour" : $util.urlDecode($input.params('hour'))
+    }
+    EOF
+  }
+}
+
+resource "aws_api_gateway_method" "number_rootpy" {
+  rest_api_id   = aws_api_gateway_rest_api.rest_api.id
+  resource_id   = aws_api_gateway_rest_api.rest_api.root_resource_id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda_rootpy" {
+  rest_api_id = aws_api_gateway_rest_api.rest_api.id
+  resource_id = aws_api_gateway_method.number_rootpy.resource_id
+  http_method = aws_api_gateway_method.number_rootpy.http_method
+
+  integration_http_method = "POST"
+  type                    = "AWS"
+  uri                     = aws_lambda_function.lambda_function.invoke_arn
+}
+
+resource "aws_api_gateway_method_response" "response_200" {
+  rest_api_id = aws_api_gateway_rest_api.rest_api.id
+  resource_id = aws_api_gateway_resource.number.id
+  http_method = aws_api_gateway_method.number.http_method
+  status_code = "200"
+
+  response_models = { "application/json" = "Empty" }
+}
+
+resource "aws_api_gateway_integration_response" "IntegrationResponse" {
+  depends_on = [
+    aws_api_gateway_integration.lambdapy,
+    aws_api_gateway_integration.lambda_rootpy,
+  ]
+  rest_api_id = aws_api_gateway_rest_api.rest_api.id
+  resource_id = aws_api_gateway_resource.number.id
+  http_method = aws_api_gateway_method.number.http_method
+  status_code = aws_api_gateway_method_response.response_200.status_code
+  # Transforms the backend JSON response to json. The space is "A must have"
+  response_templates = {
+    "application/json" = <<EOF
+  EOF
+  }
+}
+
+resource "aws_api_gateway_deployment" "gateway_deployment" {
+  depends_on = [
+    aws_api_gateway_integration.lambdapy,
+    aws_api_gateway_integration_response.IntegrationResponse,
+  ]
+
+  rest_api_id = aws_api_gateway_rest_api.rest_api.id
+  stage_name  = var.stage
+
+}
+
+# ********************************************************************************************************************
+# resource "aws_api_gateway_resource" "proxy" {
+#   rest_api_id = aws_api_gateway_rest_api.rest_api.id
+#   parent_id   = aws_api_gateway_rest_api.rest_api.root_resource_id
+#   path_part   = "{proxy+}"
+# }
+
+# resource "aws_api_gateway_method" "proxy" {
+#   rest_api_id   = aws_api_gateway_rest_api.rest_api.id
+#   resource_id   = aws_api_gateway_resource.proxy.id
+#   http_method   = "ANY"
+#   authorization = "NONE"
+# }
+
+# resource "aws_api_gateway_integration" "lambda" {
+#   rest_api_id = aws_api_gateway_rest_api.rest_api.id
+#   resource_id = aws_api_gateway_method.proxy.resource_id
+#   http_method = aws_api_gateway_method.proxy.http_method
+
+#   integration_http_method = "POST"
+#   type                    = "AWS_PROXY"
+#   uri                     = aws_lambda_function.lambda_function.invoke_arn
+# }
+
+# resource "aws_api_gateway_method" "proxy_root" {
+#   rest_api_id   = aws_api_gateway_rest_api.rest_api.id
+#   resource_id   = aws_api_gateway_rest_api.rest_api.root_resource_id
+#   http_method   = "ANY"
+#   authorization = "NONE"
+# }
+
+# resource "aws_api_gateway_integration" "lambda_root" {
+#   rest_api_id = aws_api_gateway_rest_api.rest_api.id
+#   resource_id = aws_api_gateway_method.proxy_root.resource_id
+#   http_method = aws_api_gateway_method.proxy_root.http_method
+
+#   integration_http_method = "POST"
+#   type                    = "AWS_PROXY"
+#   uri                     = aws_lambda_function.lambda_function.invoke_arn
+# }
+
+# resource "aws_api_gateway_deployment" "gateway_deployment" {
+#   depends_on = [
+#     aws_api_gateway_integration.lambda,
+#     aws_api_gateway_integration.lambda_root,
+#   ]
+
+#   rest_api_id = aws_api_gateway_rest_api.rest_api.id
+#   stage_name  = "test"
+# }
+
+# *************************************************************************************************
+
+output "base_url" {
+  value = "${aws_api_gateway_deployment.gateway_deployment.invoke_url}/${var.resource_name}"
+}
+
+resource "aws_lambda_permission" "lambda_permission" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.lambda_function.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  # The /*/* portion grants access from any method on any resource
+  # within the API Gateway "REST API".
+  source_arn = "${aws_api_gateway_rest_api.rest_api.execution_arn}/*/*"
+}
+
+resource "aws_sqs_queue" "queue" {
+  name = "s3-event-notification-queue"
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "sqs:SendMessage",
+      "Resource": "arn:aws:sqs:*:*:s3-event-notification-queue",
+      "Condition": {
+        "ArnEquals": { "aws:SourceArn": "${aws_s3_bucket.bucket.arn}" }
+      }
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = aws_s3_bucket.bucket.id
+
+  queue {
+    queue_arn     = aws_sqs_queue.queue.arn
+    events        = ["s3:ObjectCreated:*"]
+    filter_suffix = ".log"
+  }
+}
---
+++ iac/variable.tf
@@ -0,0 +1,11 @@
+variable "region" {
+  default = "us-west-1"
+}
+
+variable "resource_name" {
+  default = "number"
+}
+
+variable "stage" {
+  default = "dev"
+}
