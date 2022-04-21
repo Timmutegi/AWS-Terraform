107f10d232d64dedc7a45faca15f74a372eae601
Timmutegi <mutegi.timothy538@gmail.com>
2022-04-21 09:36:57 -0600

    removed aclarion

--- iac/main.tf
+++ iac/main.tf
@@ -15,7 +15,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "bucket" {
-  bucket = "aclarion-bucket-1"
+  bucket = "tesh-mutegi-bucket"
 }
 
 resource "aws_iam_role" "iam_for_lambda" {
@@ -82,14 +82,14 @@ resource "aws_lambda_function" "lambda_function" {
   # If the file is not in the current working directory you will need to include a 
   # path.module in the filename.
   filename      = "${path.module}/lambda_function/hello-python.zip"
-  function_name = "AclarionFunction"
+  function_name = "TeshMutegiFunction"
   role          = aws_iam_role.iam_for_lambda.arn
   handler       = "handler.lambda_handler"
   runtime       = "python3.9"
 }
 
 resource "aws_api_gateway_rest_api" "rest_api" {
-  name        = "AclarionAPI"
+  name        = "TeshMutegiAPI"
   description = "This API is an event source for the lambda function"
 }
 
