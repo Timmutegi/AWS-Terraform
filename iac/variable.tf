7d2f141cd59de2fd590526687a940af2af794816
Timmutegi <mutegi.timothy538@gmail.com>
2022-07-13 18:41:47 -0600

    Added variables

--- iac/variable.tf
+++ iac/variable.tf
@@ -1,6 +1,6 @@
 variable "region" {
   type    = string
-  default = "us-west-1"
+  default = "us-east-1"
 }
 
 variable "resource_name" {
@@ -13,11 +13,30 @@ variable "stage" {
   default = "dev"
 }
 
+variable "lambda_function_name" {
+  type        = string
+  description = "Name of the lambda function"
+  default     = "my_lambda_function"
+}
+
+variable "rest_api_name" {
+  type        = string
+  description = "Name of the REST API"
+  default     = "my_rest_api"
+}
+
+variable "bucket_name" {
+  type        = string
+  description = "Name of the S3 bucket"
+  default     = "tesh-mutegi-bucket"
+}
+
 #AWS authentication variables
 variable "aws_access_key" {
   type        = string
   description = "AWS Access Key"
 }
+
 variable "aws_secret_key" {
   type        = string
   description = "AWS Secret Key"
